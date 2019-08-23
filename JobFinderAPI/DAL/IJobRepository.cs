4f54e961effd018e56dcc9e8310bea1fb7cdc2ec
Prasham Jhaveri <prashamjhaveri@Prashams-MacBook-Air.local>
2019-08-24 11:01:09 +1200

    swagger not working

---
+++ JobFinderAPI/DAL/IJobRepository.cs
@@ -0,0 +1,18 @@
+﻿using JobfFinderAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobfFinderAPI.DAL
+{
+    public interface IJobRepository : IDisposable
+    {
+        IEnumerable<Jobs> GetJobs();
+        Jobs GetJobsByID(int JobId);
+        void InsertJob(Jobs job);
+        void DeleteJob(int JobId);
+        void UpdateJob(Jobs job);
+        void Save();
+    }
+}
---
+++ JobFinderAPI/Model/MapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace JobfFinderAPI.Model
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Jobs, JobsDTO>();
+            CreateMap<JobsDTO, Jobs>();
+        }
+    }
+}
